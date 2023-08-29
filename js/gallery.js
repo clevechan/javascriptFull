8ec77405d07cdbf9e906fd8b60ef4ad113378020
Cleve <clevechan@gmail.com>
2023-08-30 02:39:18 +0800

    complete the full javascript gallery site

--- js/gallery.js
+++ js/gallery.js
@@ -2,15 +2,30 @@
 // The main task is to attach an event listener to each image in the gallery
 // and respond appropriately on click.
 function activateGallery() {
-  let thumbnails = document.querySelector("#gallery-thumbs").
-                            querySelectorAll("img");
-  let mainImage = document.querySelector("#gallery-photo img");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage  = document.querySelector("#gallery-photo img");
 
   thumbnails.forEach(function(thumbnail) {
+    // Preload large images.
+    let newImageSrc  = thumbnail.dataset.largeVersion;
+    let largeVersion = new Image();
+    largeVersion.src = newImageSrc;
+
     thumbnail.addEventListener("click", function() {
-      // Set clicked image as main image.
-      let newImageSrc = thumbnail.dataset.largeVersion;
+      // Set clicked image as display image.
       mainImage.setAttribute("src", newImageSrc);
+
+      // Change which image is current.
+      document.querySelector(".current").classList.remove("current");
+      thumbnail.parentNode.classList.add("current");
+
+      // Update image info.
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title       = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+
+      title.innerHTML       = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
     });
   });
 }
